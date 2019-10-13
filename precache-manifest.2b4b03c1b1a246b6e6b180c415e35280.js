88354726d8c9c72e9aff8c8253caaf4d9003aca5
KristinaPeregudova <perekrist31@gmail.com>
2019-10-13 13:45:44 +0700

    Updates

---
+++ precache-manifest.2b4b03c1b1a246b6e6b180c415e35280.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6298e59a950b2f7d2559bd93fb8d3c06",
+    "url": "./index.html"
+  },
+  {
+    "revision": "bbe25c1303e1048e3971",
+    "url": "./static/css/2.70f3b80d.chunk.css"
+  },
+  {
+    "revision": "f8a1d147c972553aea3f",
+    "url": "./static/css/main.5294250c.chunk.css"
+  },
+  {
+    "revision": "bbe25c1303e1048e3971",
+    "url": "./static/js/2.bdd1b9c6.chunk.js"
+  },
+  {
+    "revision": "f8a1d147c972553aea3f",
+    "url": "./static/js/main.20488aca.chunk.js"
+  },
+  {
+    "revision": "0b2e966c9d6b1c18d3f2",
+    "url": "./static/js/runtime-main.1d945cf4.js"
+  },
+  {
+    "revision": "bc92f5d1ae81ce9740b7b037f0e21a71",
+    "url": "./static/media/nastya16.bc92f5d1.jpeg"
+  }
+]);
